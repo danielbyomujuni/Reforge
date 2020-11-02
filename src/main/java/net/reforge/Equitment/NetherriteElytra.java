5f21bd7cf0b2c7bd7880f3194724cbb0026e68f3
Daniel <trainerlord@gmail.com>
2020-11-02 08:26:19 -0500

    PC Backup Commit

---
+++ src/main/java/net/reforge/Equitment/NetherriteElytra.java
@@ -0,0 +1,20 @@
+package net.reforge.Equitment;
+
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.ArmorMaterial;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import net.reforge.Reforge;
+
+public class NetherriteElytra extends Item {
+
+    public NetherriteElytra(Settings settings) {
+        super(settings);
+        // TODO Auto-generated constructor stub
+    }
+
+    
+}
--- src/main/java/net/reforge/Reforge.java
+++ src/main/java/net/reforge/Reforge.java
@@ -3,24 +3,23 @@ package net.reforge;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.ArmorItem;
+import net.minecraft.item.ArmorMaterial;
 import net.minecraft.item.ArmorMaterials;
-import net.minecraft.item.ElytraItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+import net.reforge.Equitment.NetherriteElytra;
 
 public class Reforge implements ModInitializer {
+    public static final Item NETHERITE_ELYTRA = new Item(new Item.Settings().group(ItemGroup.TRANSPORTATION));
+    @Override
+    public void onInitialize() {
+        // TODO Auto-generated method stub
 
-	public static final Item NetherElytra = new ElytraItem(new Item.Settings().group(ItemGroup.COMBAT).fireproof().maxCount(1).maxDamage(100));
-	
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-		//Items
-		Registry.register(Registry.ITEM, new Identifier("reforge", "netherrite_elytra"), NetherElytra);
+        Registry.register(Registry.ITEM, new Identifier("reforge", "netherite_elytra"), NETHERITE_ELYTRA);
+            }
 
-	}
+
+    
 }
